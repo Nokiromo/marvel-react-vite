380295e2dee091688b6b5c62bfbc86627b3913f7
Oleksandr Nokiroma <sashamorykon@gmail.com>
2025-10-04 15:00:44 +0300

    created the basic structure of the project

---
+++ src/components/appBanner/AppBanner.jsx
@@ -0,0 +1,15 @@
+import './appBanner.scss'
+
+const AppBanner = () =>{
+    return(
+        <div className="app__banner">
+            <img src='avengers' alt='Avengers'img/>
+            <div className="app__banner-text">
+                New comics every week!<br/>
+                Stay tuned!
+            </div>
+            <img src="avengersLogo" alt="Avengers logo" />
+        </div>
+    )
+}
+export default AppBanner
