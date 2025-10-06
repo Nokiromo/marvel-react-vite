0c5a451c06f817a21a8c878ded44e37bee9823d8
Oleksandr Nokiroma <sashamorykon@gmail.com>
2025-10-06 23:02:50 +0300

    Refactor RandomChar: move updateChar to componentDidMount and fix trim bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,7 @@ import App from "./components/app/App";
 
 import "./style/style.scss";
 
-import MarvelService from './services/MarvelService';
 
-const marvelService = new MarvelService()
-
-marvelService.getAllCharacters(1011052).then(res => res.data.results.forEach(item => {return console.log(item.name)
-  
-}))
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
